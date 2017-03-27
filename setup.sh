e9bf2026b9f7910f712f640d85acb956ac591abe
Ray Chen <rayruichen@gmail.com>
2017-03-27 19:19:33 -0400

    update setup

--- setup.sh
+++ setup.sh
@@ -3,13 +3,9 @@ dir=$(dirname "$0")
 echo "What is the url of your project?"
 read url
 
-pushd $dir
-
-git remote set-url origin url
+git -C $dir remote set-url origin $url
 
 echo "Remotes set to:"
-git remote -v
+git -C $dir remote -v
 
 rm -- "$0"
-
-popd
